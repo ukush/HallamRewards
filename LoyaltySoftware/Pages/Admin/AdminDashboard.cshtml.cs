940d4007cb74484c10ea6170fb32407d5210f3fd
ukush <64285005+ukush@users.noreply.github.com>
2021-03-09 14:51:44 +0000

    "fixed bugs in dashboard/session"

---
+++ LoyaltySoftware/Pages/Admin/AdminDashboard.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoyaltySoftware.Pages.AdminPages
+{
+    public class AdminDashboardModel : PageModel
+    {
+        public string Username;
+
+        public const string SessionKeyName1 = "username";
+
+        public string SessionID;
+        public const string SessionKeyName3 = "sessionID";
+
+        public IActionResult OnGet()
+        {
+            Username = HttpContext.Session.GetString(SessionKeyName1);
+            SessionID = HttpContext.Session.GetString(SessionKeyName3);
+
+            if (string.IsNullOrEmpty(Username) && string.IsNullOrEmpty(SessionID))
+            {
+                return RedirectToPage("/Login/UserLogin");
+            }
+            return Page();
+
+        }
+    }
+}
--- LoyaltySoftware/Pages/Member/MemberDashboard.cshtml.cs
+++ LoyaltySoftware/Pages/Member/MemberDashboard.cshtml.cs
@@ -1,16 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using LoyaltySoftware.Models;
+using LoyaltySoftware.Pages.Shared;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace LoyaltySoftware.Pages.AdminPages
+namespace LoyaltySoftware.Pages.Login
 {
-    public class dashboardModel : PageModel
+    public class MemberDashboardModel : PageModel
     {
         public string Username;
+
         public const string SessionKeyName1 = "username";
 
         public string SessionID;
@@ -30,3 +34,4 @@ namespace LoyaltySoftware.Pages.AdminPages
         }
     }
 }
+
