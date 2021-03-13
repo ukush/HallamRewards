3f528babf6dfe978f940f491d33ca1dc7e53363f
ukush <64285005+ukush@users.noreply.github.com>
2021-03-13 13:24:40 +0000

    updated login messge

--- LoyaltySoftware/Pages/Login/UserLogin.cshtml.cs
+++ LoyaltySoftware/Pages/Login/UserLogin.cshtml.cs
@@ -15,69 +15,88 @@ namespace LoyaltySoftware.Pages.Login
     {
         [BindProperty]
         public UserAccount UserAccount { get; set; }
-        public string Message { get; set; }
+
+        [BindProperty]
+        public Userdbo UserRec { get; set; }
+
+
+
+        public string Message1 { get; set; }
+        public string Message2 { get; set; }
+
+        public string Message3 { get; set; }
         public string SessionID;
+
+        public int LoginAttempts { get; set; }
+
+        public int currentattemps;
+
 
 
         public void OnGet()
         {
+
         }
 
         public IActionResult OnPost()
         {
-            DBConnection dbstring = new DBConnection(); //creating an object from the class
-            string DbConnection = dbstring.DatabaseString(); //calling the method from the class
-            Console.WriteLine(DbConnection);
-            SqlConnection conn = new SqlConnection(DbConnection);
-            conn.Open();
 
-            Console.WriteLine(UserAccount.username);
-            Console.WriteLine(UserAccount.password);
+                // need to pass on user_id to next page
 
-            if (string.IsNullOrEmpty(UserAccount.username))
-            {
-                Message = "Please enter a username!";
-                return Page();
-            }
-            else if (string.IsNullOrEmpty(UserAccount.password))
-            {
-                Message = "Please enter a password!";
-                return Page();
-            }
-            else
-            {
 
-                if (UserAccount.checkIfUsernameExists(UserAccount.username))
+                DBConnection dbstring = new DBConnection(); //creating an object from the class
+                string DbConnection = dbstring.DatabaseString(); //calling the method from the class
+                Console.WriteLine(DbConnection);
+                SqlConnection conn = new SqlConnection(DbConnection);
+                conn.Open();
+
+                Console.WriteLine(UserAccount.username);
+                Console.WriteLine(UserAccount.password);
+
+
+                if (string.IsNullOrEmpty(UserAccount.username))
                 {
-                    SessionID = HttpContext.Session.Id;
-                    HttpContext.Session.SetString("sessionID", SessionID);
-                    HttpContext.Session.SetString("username", UserAccount.username);
-                    HttpContext.Session.SetString("password", UserAccount.password);
-
-                    if (!UserAccount.checkPassword(UserAccount.username, UserAccount.password))
-                    {
-                        Message = "Password does not match!";
-                        return Page();
-                    }
-                    else
-                    {
-                        if (UserAccount.checkRole(UserAccount.username) == "member")
-                        {
-                            return RedirectToPage("/Member/MemberDashboard");
-                        }
-                        else
-                        {
-                            return RedirectToPage("/Admin/AdminDashboard");
-                        }
-                    }
+                    Message1 = "Please enter a username!";
+                    return Page();
+                }
+                else if (string.IsNullOrEmpty(UserAccount.password))
+                {
+                    Message2 = "Please enter a password!";
+                    return Page();
                 }
                 else
                 {
-                    Message = "Username does not exist!";
-                    return Page();
-                }
-            }
 
+                    if (UserAccount.checkIfUsernameExists(UserAccount.username))
+                    {
+                        SessionID = HttpContext.Session.Id;
+                        HttpContext.Session.SetString("sessionID", SessionID);
+                        HttpContext.Session.SetString("username", UserAccount.username);
+                        HttpContext.Session.SetString("password", UserAccount.password);
+
+                        if (!UserAccount.checkPassword(UserAccount.username, UserAccount.password))
+                        {
+                            Message2 = "Password does not match!";
+                            return Page();
+                    }
+                        else
+                        {
+                            if (UserAccount.checkRole(UserAccount.username) == "member")
+                            {
+                                return RedirectToPage("/Member/MemberDashboard");
+                            }
+                            else
+                            {
+                                return RedirectToPage("/Admin/AdminDashboard");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Message1 = "Username does not exist!";
+                        return Page();
+                    }
+                }
             
         }
     }
