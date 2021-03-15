0bc666fd4c69fa9a74e278f5690c15793b0461dc
muhammad2000861 <75323053+muhammad2000861@users.noreply.github.com>
2021-03-15 16:54:43 +0000

    pageSettingsRewards

--- LoyaltySoftware/Pages/Reward/Reward.cshtml.cs
+++ LoyaltySoftware/Pages/Reward/Reward.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace LoyaltySoftware.Pages.Shop
+namespace LoyaltySoftware.Pages.Reward
 {
     public class ShopModel : PageModel
     {
