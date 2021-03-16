352e5c4d0cbcf61260c4dc3cfb5518acf5fc7fd7
Kemas Raihan <raihan.madani@gmail.com>
2021-03-16 17:07:47 +0000

    Updated Product.cs

--- LoyaltySoftware/Models/Product.cs
+++ LoyaltySoftware/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,16 @@ namespace LoyaltySoftware.Models
 {
     public class Product
     {
-        public int Id { get; set; }
+        [Display (Name = "Product ID")]
         public int productId { get; set; }
+        [Display(Name = "Image")]
+        public string productImageSrc { get; set; }
+        [Display (Name = "Name")]
         public string productName { get; set; }
-        public int price { get; set; }
-        public string description { get; set; }
+        [Display (Name = "Price")]
+        public string productPrice { get; set; }
+
+        [Display(Name = "Description")]
+        public string productDescription { get; set; }
     }
 }
