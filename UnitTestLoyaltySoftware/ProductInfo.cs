5b8f0b2c7bea2cbce166cd36bc6e03efe7318613
Kemas Raihan <raihan.madani@gmail.com>
2021-03-17 19:51:23 +0000

    Added unit testing for ProductInfo
    Added files for unit testing ProductInfo.cshtml.cs methods.

---
+++ UnitTestLoyaltySoftware/ProductInfo.cs
@@ -0,0 +1,54 @@
+﻿using LoyaltySoftware.Models;
+using LoyaltySoftware.Pages.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoyaltySoftware.UnitTests
+{
+    public class ProductInfo
+    {
+        [BindProperty]
+        public static Product ProductRec { get; set; }
+        public static string getProductName(int? id)
+        {
+            DBConnection dbstring = new DBConnection();
+            string DbConnection = dbstring.DatabaseString();
+            Console.WriteLine(DbConnection);
+            SqlConnection conn = new SqlConnection(DbConnection);
+            conn.Open();
+
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = conn;
+                command.CommandText = "SELECT * FROM Product WHERE product_id = @PID";
+
+                command.Parameters.AddWithValue("@PID", id);
+                Console.WriteLine("The id : " + id);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ProductRec.productId = reader.GetInt32(0);
+                    ProductRec.productName = reader.GetString(1);
+                    ProductRec.productPrice = (double)reader.GetDecimal(2);
+                    ProductRec.productImageSrc = reader.GetString(3);
+                    ProductRec.productDescription = reader.GetString(4);
+                }
+
+            }
+
+            return ProductRec.productName;
+        }
+
+        public int calculatePointsEarned(double price)
+        {
+            return (int)Math.Round(ProductRec.productPrice, 0);  // price of the product is converted to points where is it is rounded to the nearest integer
+        }
+
+    }
+}
