a0a0cf21ccc7372dd35c38d4a5c0a3a5cb13a424
Kemas Raihan <raihan.madani@gmail.com>
2021-03-20 01:21:03 +0000

    Updated test files

---
+++ UnitTestLoyaltySoftware/UnitTestClaimReward.cs
@@ -0,0 +1,26 @@
+﻿using LoyaltySoftware.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestLoyaltySoftware
+{
+    [TestClass]
+    public class UnitTestClaimReward
+    {
+        [TestMethod]
+        public void TestGetReward()
+        {
+            
+
+            //Assert.AreEqual(productNameExpected, productNameActual);
+        }
+
+        [TestMethod]
+        public void TestPointsRemoved()
+        {
+
+        }
+    }
+}
